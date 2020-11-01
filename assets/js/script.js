ce47620118ff8ea21e2f3fa2ddf34336421d2bc6
Ahmed Guetti <guetti4ahmed@gmail.com>
2020-11-02 00:13:19 +0100

    make toggle btn working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-// Make responsive menu button moving
+
 const menu = document.querySelector(".menu");
+const navBar = document.querySelector(".navBar");
 menu.addEventListener('click', function(){
+    // Make responsive menu button moving
     this.classList.toggle("is-active");
+    //Make nav bar move forward
+    navBar.classList.toggle("active");
 });
+
+
+
